2dbf4b26fb95569ac2064e0e2d1edce6292ee8d3
Дмитрий Гришин <dmitrijgrisin@MacBook-Pro-Dmitrij.local>
2022-08-02 17:02:13 +0300

    add Prime-game

---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+import {startGame } from './index.js';
+import { getRandomInt } from './even.js';
+
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getPrimeNumber = (number) => {
+  for(let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+    return false;
+}
+}
+return true;
+};
+
+const generateRound = () => {
+  const number = getRandomInt(2, 100);
+  const userQuestion = number;
+  const rightAnswer = (getPrimeNumber(number)) ? 'yes' : 'no';
+  return [userQuestion, rightAnswer];
+}
+
+const evenGame = () => startGame(gameDescription, generateRound);
+
+export default evenGame;
