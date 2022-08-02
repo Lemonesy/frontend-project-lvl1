95a2d440ad12e8fd9c34ae7c996565a6cfc58e23
Дмитрий Гришин <dmitrijgrisin@MacBook-Pro-Dmitrij.local>
2022-08-02 17:52:51 +0300

    fix even game

--- src/games/even.js
+++ src/games/even.js
@@ -5,7 +5,7 @@ const gameDescription = 'Answer "yes" if the number is even, otherwise answer "n
 export const getRandomInt = (min, max) => Math.round(Math.random() * (max - min)) + min;
 
 const generateRound = () => {
-  const userQuestion = getRandomInt(100);
+  const userQuestion = getRandomInt(1, 100);
   const rightAnswer = (userQuestion % 2 === 0) ? 'yes' : 'no';
   return [userQuestion, rightAnswer];
 };
